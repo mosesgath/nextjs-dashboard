8ff4efceb3d70de0bf2e48a012901b743e54b0ac
moses <mosesgath9@gmail.com>
2024-05-20 00:16:53 +0300

    nextjs-dashboard

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,3 @@
 export default function Invoices() {
-    return <p className="text-xl flex flex-row justify-center font-bold">Invoices Page</p>;
+    return <p className="text-xl flex flex-row justify-center font-bold">Invoi43ces Page</p>;
   }
